1b168d0e007b25701e22a9b0a207f4c83a05cb96
krdavis5 <krdavis5@wisc.edu>
2023-06-10 09:48:14 -0500

    Data sorted into JSONArray list

--- app/src/main/java/com/example/fetch/MainActivity.java
+++ app/src/main/java/com/example/fetch/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,6 +21,7 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.Buffer;
 import java.nio.channels.AsynchronousChannel;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
@@ -34,7 +36,14 @@ public class MainActivity extends AppCompatActivity {
     static Boolean flag = false;
     static Boolean errorOccured = false;
     static String dataStream = "";
-    static JSONArray arr = new JSONArray();
+    static JSONArray arr1 = new JSONArray();
+    static JSONArray arr2 = new JSONArray();
+    static JSONArray arr3 = new JSONArray();
+    static JSONArray arr4 = new JSONArray();
+
+    static JSONArray[] sorted = new JSONArray[4];
+
+    static int removed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,23 +51,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("points", "one");
         textView = findViewById(R.id.textView);
-//        try {
-            data();
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
+        sorted[0] = arr1;
+        sorted[1] = arr2;
+        sorted[2] = arr3;
+        sorted[3] = arr4;
+
+        data();
         int i = 0;
         while (!flag) {
             i++;
         }
-        Log.d("points", String.valueOf(i));
-
+        dataParse();
+        Log.d("d", String.valueOf(removed));
         if(errorOccured){
             String errMess = "An error has occurred, please start the app";
             textView.setText(errMess);
         } else {
             textView.setText(dataStream);
         }
+
+
+    }
+
+    private void dataParse() {
+        int counter = 0;
+for(int j = 0; j < sorted.length; j++){
+        for(int i = 0; i < sorted[j].length(); i++) {
+            try {
+                JSONObject obj = sorted[j].getJSONObject(i);
+                counter++;
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        }
+        Log.d("counter", String.valueOf(counter));
 
 
     }
@@ -77,11 +104,23 @@ public class MainActivity extends AppCompatActivity {
                     URLConnection fetchData = url.openConnection();
                     in = new BufferedReader(new InputStreamReader(fetchData.getInputStream()));
 
-
+                    JSONObject job;
 
                     String inputLine = in.readLine();
                     while (inputLine != null) {
-                        dataStream += '\n' + inputLine;
+                        if(!inputLine.equals("[") && !inputLine.equals("]")) {
+                            dataStream += '\n' + inputLine;
+                            job = (JSONObject) new JSONTokener(inputLine).nextValue();
+                            int id = (int) job.get("listId");
+                            if(!job.get("name").equals(null) && !job.get("name").equals("")){
+                                sorted[id - 1].put(job);
+                            } else {
+                                removed++;
+                            }
+
+                        } else {
+                            Log.d("symbol", inputLine);
+                        }
                         inputLine = in.readLine();
                     }
 
@@ -92,6 +131,8 @@ public class MainActivity extends AppCompatActivity {
                     Log.e("Error", "IO Exception");
                     errorOccured = true;
 
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
                 }
                 flag = true;
 
